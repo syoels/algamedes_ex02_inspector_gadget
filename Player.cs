320ecac0fc010e67a704041925b348fc9270cfc3
syoels <syoels@gmail.com>
2017-03-17 14:47:51 +0200

    Ex files

---
+++ Player.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using Infra;
+using Infra.Gameplay;
+using Infra.Utils;
+
+namespace Gadget {
+
+[RequireComponent(typeof(Animator), typeof(Rigidbody2D), typeof(OverlapChecker))]
+/// <summary>
+/// The player controller class.
+/// </summary>
+public class Player : MonoBehaviour {
+
+		// Character parameteres
+		public int health = 1;
+		[Range(0f,40f)]
+		[SerializeField] float jumpHeight = 15f;
+		[SerializeField] float xSpeed = 7f;
+		[SerializeField] float gravity = 2f;
+		[Range(0f,360f)]
+		[SerializeField] float shootAngle = 45f;
+		[SerializeField] float bulletSpeed = 15f;
+
+		// Character Controls
+		KeyCode KEYRIGHT = KeyCode.D;
+		KeyCode KEYLEFT = KeyCode.A;
+		KeyCode KEYUP  = KeyCode.W;
+		KeyCode KEYSHOOT = KeyCode.Space;
+
+		[SerializeField] Transform arm; //set to BoneArm
+		[SerializeField] Transform gun;
+		[SerializeField] Rigidbody2D currBullet;
+
+		private readonly int animatorIsAlive = Animator.StringToHash("Alive");
+		private readonly int animatorJumpTrigger = Animator.StringToHash("Jump");
+
+		private Animator animator;
+			private Rigidbody2D playerBody;
+			private OverlapChecker floorOverlapChecker;
+
+	    private bool isOnFloor {
+	        get {
+				return floorOverlapChecker.isOverlapping;
+	        }
+	    }
+				
+	    protected void Awake() {
+	        animator = GetComponent<Animator>();
+			playerBody = GetComponent<Rigidbody2D>();
+	        floorOverlapChecker = GetComponent<OverlapChecker>();
+	        animator.SetBool(animatorIsAlive, true);
+	    }
+				
+	    protected void Update() {
+
+			// Set arm angle for shooting
+			var armAngle = arm.eulerAngles;
+	        armAngle.z = shootAngle;
+	        arm.eulerAngles = armAngle;
+
+	        playerBody.gravityScale = gravity;
+
+			var currSpeed = playerBody.velocity;
+
+			// Jump, move, shoot
+	        if (Input.GetKeyDown(KEYUP) && isOnFloor) {
+	            currSpeed.y = jumpHeight;
+	            playerBody.velocity = currSpeed;
+	            animator.SetTrigger(animatorJumpTrigger);
+	        } else if (Input.GetKey(KEYRIGHT)) {
+	            currSpeed.x = xSpeed;
+	            playerBody.velocity = currSpeed;
+	        } else if (Input.GetKey(KEYLEFT)) {
+	            currSpeed.x = -xSpeed;
+	            playerBody.velocity = currSpeed;
+			} else if (Input.GetKey(KEYSHOOT)) {
+	            if (!currBullet.gameObject.activeInHierarchy) {
+	                currBullet.gameObject.SetActive(true);
+	                currBullet.position = gun.position;
+	                currBullet.velocity = Vector2.right.Rotate(Mathf.Deg2Rad * shootAngle) * bulletSpeed;
+	            }
+	        }
+	    }
+				
+	    protected void OnCollisionEnter2D(Collision2D collision) {
+	        if (health <= 0) return;
+
+			// Reached yellow square
+	        if (collision.gameObject.CompareTag("Victory")) {
+	            DebugUtils.Log("Great Job!");
+	            return;
+	        }
+				
+	        if (!collision.gameObject.CompareTag("Enemy")) return;
+
+			// Hit by enemy. 
+	        --health;
+	        if (health > 0) return;
+
+			// Dead
+	        animator.SetBool(animatorIsAlive, false);
+	        playerBody.velocity = Vector2.zero;
+	        playerBody.gravityScale = 4f;
+	        enabled = false;
+	    }
+	}
+}
---
+++ README.txt
@@ -0,0 +1,13 @@
+Yoel Simhovitch
+303092951
+
+To complete the level I took the following steps:
+1. Jump on "SmallWall" by increasing jump jumpHeight to ~17f, using "W" (jump) & "D" (right)
+2. Jump on "SmallPlatform" (the first one) by increasing jump jumpHeight to ~20f (using "W" & "D")
+3. Change "shootAngle" to 0f
+4. Move right ("D") to fall on "Floor" while "Enemy" (the first one) is not present
+5. Shoot using spacebar
+6. Stand under "WidePlatform" (the first one) and change gravity to -0.3f
+7. Move right until end of platforms, and then quickly mobe left ("A") until not under spikes. 
+8. Change "bulletSpeed" to 40, keep floating and when in range shoot twice to kill both "Enemy"s
+9. Move right ("D") until edge of screen, change gravity to positive number, fall down and move left ("A") to yellow square.
